92ac535df82b29b12a99ecf0731d5bb05fa6ff0b
ManuelLanza122 <104244097+ManuelLanza122@users.noreply.github.com>
2022-11-29 11:27:12 -0500

    Create config.py

---
+++ tg_bot/config.py
@@ -0,0 +1,12 @@
+from tg_bot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 5516142677
+    OWNER_USERNAME = mikeysanho
+    API_KEY = RWDRIS-BMPHHE-RRCZAZ-AYKMIF-ARQ
+    SQLALCHEMY_DATABASE_URI = mongodb+srv://base:1234@cluster0.xcaad56.mongodb.net/?retryWrites=true&w=majority
+    MESSAGE_DUMP = -771072750
+    USE_MESSAGE_DUMP = True
+    SUDO_USERS = 921396580, 954299697, 5010209835, 5676170494, 5492224551, 5516142677
+    LOAD = []
+    NO_LOAD = ['translation']
